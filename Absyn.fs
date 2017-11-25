195d89888a21f9d61a5226b73cd5fb4ae7c7bea2
F10w3e <308370346@qq.com>
2017-11-25 21:22:03 +0800

    Absyn Lex Par update

---
+++ Absyn.fs
@@ -0,0 +1,20 @@
+(* File Expr/Absyn.fs
+   Abstract syntax for the simple expression language 
+ *)
+
+module Absyn
+
+type expr = 
+  | CstI of int
+  | Var of string
+  | Let of string * expr * expr
+  | Prim of string * expr * expr
+
+type sinstr =
+  | SCstI of int                        (* push integer           *)
+  | SVar of int                         (* push variable from value stack to  env stack *)
+  | SAdd                                (* pop args, push sum     *)
+  | SSub                                (* pop args, push diff.   *)
+  | SMul                                (* pop args, push product *)
+  | SPop                                (* pop value/unbind var   *)
+  | SSwap;;                             (* exchange top and next  *)
