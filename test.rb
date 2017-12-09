3db96cbe87015d4dfe1ebd4364bb871c4b79ab0e
F10w3e <308370346@qq.com>
2017-12-09 23:18:44 +0800

    lex yacc又更新
    目前无法实现函数里面定义函数

--- test.rb
+++ test.rb
@@ -1,4 +1,47 @@
 def test(x){
+	test(1);
 	return x;
+	return 1 and 2;
+	return null;
 }
 a = test(1);
+try{
+	a=1;
+}catch{
+	a=1;
+}finally{
+	a=1;
+}
+for(x in range(2))
+{
+	try{
+		a=1;
+	}catch{
+		a=1;
+	}finally{
+		a=1;
+	}
+	a = test(2);
+	if(x>2){
+		a = test(3);
+	}else{
+		a = test(5);
+	}
+	switch(x){
+		case(1):{
+			a=1;
+			c=2;}
+		default:{
+			a=2;
+		}	
+	}
+	while(x){
+		a=1;
+		break;
+	}
+	a = test(1);
+	lambda x:x+1;
+	print(1);
+	true;
+}
+import name;
